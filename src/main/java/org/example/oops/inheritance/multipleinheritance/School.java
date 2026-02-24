5aafd57f1832a4ff6814336c3be485254ac63e20
Anshu <anshu_jain@epam.com>
2026-02-24 11:55:33 +0530

    multiple Inheritance

--- src/main/java/org/example/oops/inheritance/multipleinheritance/School.java
+++ src/main/java/org/example/oops/inheritance/multipleinheritance/School.java
@@ -1,39 +1,38 @@
 package org.example.oops.inheritance.multipleinheritance;
-class Person{
+
+interface Play{
+    void play();
+}
+
+interface Study{
+    void study();
+}
+
+class Student implements Play,Study{
     String name;
-    int age;
-
-    public void showDetails(){
-        System.out.println("name : "+name);
-        System.out.println("age : "+ age);
+    int id;
+    public void play(){
+        System.out.println(name +" is playing ");
     }
-}
-
-class Student extends Person{
-    int standard;
-
-    public void standard(){
-        System.out.println(name + " is studying in " + standard + "th Standard");
-    }
-}
-
-class TopperStudent extends Student{
-    int grade;
 
     public void study(){
-        System.out.println(standard + "th Standard with "+ grade+ " grade" );
+        System.out.println(name +" is studying ");
     }
+
+    public void studentDetails(){
+        System.out.println("name : " + name);
+        System.out.println("id : "+ id);
+    }
+
 }
 public class School {
     public static void main(String[] args){
-        TopperStudent topperStudent = new TopperStudent();
-        topperStudent.name = "Ram";
-        topperStudent.age = 15;
-        topperStudent.standard = 10;
-        topperStudent.grade = 90;
+        Student student =  new Student();
+        student.name = "Anshu";
+        student.id = 21;
 
-        topperStudent.showDetails();
-        topperStudent.standard();
-        topperStudent.study();
+        student.studentDetails();
+        student.play();
+        student.study();
     }
 }
