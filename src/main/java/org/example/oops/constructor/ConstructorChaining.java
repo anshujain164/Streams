650da8968fd496c87a4fcec8154efd78c120e6ff
Anshu <anshu_jain@epam.com>
2026-02-27 11:20:19 +0530

    constructor chaining

---
+++ src/main/java/org/example/oops/constructor/ConstructorChaining.java
@@ -0,0 +1,32 @@
+package org.example.oops.constructor;
+class Person{
+    protected String name;
+    public Person(){
+        this("unnamed");
+    }
+
+    public Person(String name){
+        this.name = name;
+        System.out.println("Person Constructor : " + name);
+    }
+}
+
+class Student extends Person{
+    private int grade;
+
+    public Student(){
+        this("unnamed " , -1);
+    }
+
+    public Student (String name , int grade){
+        super(name);
+        this.grade = grade;
+        System.out.println("constructor of Student  : "+ name + " : " + grade);
+    }
+}
+public class ConstructorChaining {
+    public static void main(String[] args){
+        new Student();
+        new Student("anshi" , 50);
+    }
+}
